65895295a64cbe67a2976fdd13177586185a45d1
Sam Ireland <sam.ireland.uk@gmail.com>
2021-07-03 23:56:45 +0100

    Change build target

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import postcss from "rollup-plugin-postcss";
 export default {
   input: "src/index.js",
   output: {
-    file: "./lib/index.js",
+    file: "./dist/index.js",
     format: "esm",
     name: "bundle"
   },
