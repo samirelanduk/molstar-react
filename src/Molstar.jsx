669fc5549bf6b07dda6a7d9e39dee52b78c062ba
Sam Ireland <sam.ireland.uk@gmail.com>
2021-08-03 20:04:08 +0100

    Allow control over viewer dimensions

--- src/Molstar.jsx
+++ src/Molstar.jsx
@@ -5,7 +5,7 @@ import { Viewer } from "molstar/build/viewer/molstar";
 
 const Molstar = props => {
 
-  const { pdbId, url, options } = props;
+  const { pdbId, url, dimensions, options } = props;
   const viewerElement = useRef(null);
   const viewer = useRef(null);
 
@@ -16,14 +16,25 @@ const Molstar = props => {
     return () => viewer.current = null;
   }, [])
 
+  if (!dimensions) return <div ref={viewerElement} />
+
   return (
-    <div ref={viewerElement} />
+    <div style={{
+      position: "relative",
+      width: dimensions[0], height: dimensions[1]
+    }}>
+      <div ref={viewerElement} style={{
+        position: "absolute",
+        width: dimensions[0], height: dimensions[1], left: 0, right: 0
+      }}/>
+    </div>
   );
 };
 
 Molstar.propTypes = {
   pdbId: PropTypes.string,
   url: PropTypes.string,
+  dimensions: PropTypes.array,
   options: PropTypes.object
 };
 
