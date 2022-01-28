e650ca0d1be136a9bde18e73f92a788d222fd9c7
Sam Ireland <mail@samireland.com>
2022-01-28 20:49:10 +0000

    Make pdbId/url work with pluginContext

--- src/Molstar.jsx
+++ src/Molstar.jsx
@@ -14,8 +14,20 @@ const Molstar = props => {
     plugin.current = new PluginContext(DefaultPluginSpec());
     plugin.current.initViewer(canvasRef.current, parentRef.current);
     await plugin.current.init();
+    await loadStructure(pdbId, url, plugin.current);
     return () => plugin.current = null;
   }, [])
+
+  const loadStructure = async (pdbId, url, plugin) => {
+    const structureUrl = url ? url : pdbId ? `https://files.rcsb.org/view/${pdbId}.cif` : null;
+    if (!structureUrl) return;
+    const data = await plugin.builders.data.download(
+      { url: structureUrl }, {state: {isGhost: true}}
+    );
+    const extension = structureUrl.split(".").pop().replace("cif", "mmcif");
+    const traj = await plugin.builders.structure.parseTrajectory(data, extension);
+    await plugin.builders.structure.hierarchy.applyPreset(traj, "default");
+  }
 
   if (!dimensions) return <div ref={pluginElement} />
 
